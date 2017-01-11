f22ff96477e4070e6395b9e28543475c450bd8ac
C Fu <C Fu>
2017-01-10 21:15:18 -0800

    Rework on search friend route, wall post

--- controllers/search_controller.js
+++ controllers/search_controller.js
@@ -44,7 +44,7 @@ router.post('/friend', function(req,res){
     {where:{id:req.body.friended}
     })
     .then(function(friend){
-        res.redirect('/search');
+        res.redirect('/home');
     })
 })
 
